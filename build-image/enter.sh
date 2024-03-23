ac2eef7785ab80c7354e5c4e1dfce624cecd2337
Paul Khuong <pvk@pvk.ca>
2024-03-23 13:47:12 -0400

    port to linux/arm64

--- build-image/enter.sh
+++ build-image/enter.sh
@@ -2,4 +2,4 @@
 
 cd $(dirname $(readlink -f "$0"))
 
-exec docker run -v "$PWD/../":/home/user/Blog --network host -ti blog
+exec docker run  --platform linux/amd64 -v "$PWD/../":/home/user/Blog:z --network host -ti --rm localhost/blog
